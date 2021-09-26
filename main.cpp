07b3bebb9e2fd75518ed5116585947e302e130aa
Ana190902 <80015105+Ana190902@users.noreply.github.com>
2021-09-25 20:20:15 -0500

    Leer imágenes con su RGB pixel por pixel

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
-#include "widget.h"
-
-#include <QApplication>
-
-int main(int argc, char *argv[])
+#include <iostream>
+#include<QImage>
+using namespace std;
+int main()
 {
-    QApplication a(argc, argv);
-    Widget w;
-    w.show();
-    return a.exec();
-}
+    string filename ="../LeerImagen/imagen/circulo.jpg";
+    QImage im (filename.c_str());
+
+    for(int indx=0;indx<im.width();++indx){
+        for(int indy=0;indy < im.height();++indy){
+            cout<<indx<<","<<indy<<"Rojo: "<<im.pixelColor(indx,indy).red()<<endl;
+            cout<<indx<<","<<indy<<"verde: "<<im.pixelColor(indx,indy).green()<<endl;
+            cout<<indx<<","<<indy<<"azul: "<<im.pixelColor(indx,indy).blue()<<endl;
+
+        }
+    }
+
+    return 0;
+};
+
