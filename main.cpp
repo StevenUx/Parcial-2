924f12506da5a20dec8b62ac71b33a0f5559af22
Ana190902 <80015105+Ana190902@users.noreply.github.com>
2021-09-27 16:24:27 -0500

    Finalizado el escalamiento de imagen y lectura RGB

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 #include<funcion.h>
 using namespace std;
 
+#define fhSize 20 // final height size
+#define fwSize 20 // final width size
+
 int main()
 {
-    string filename ="../LeerImagen/imagen/circulo.jpg";
+    string filename = "../LeerImagen/imagen/cuadrado.jpg";
     QImage im (filename.c_str());
 
 
@@ -14,10 +17,10 @@ int main()
 
     unsigned char **output_img16;
 
-    output_img16 = MemAlloc2D(16, 16, 0);
+    output_img16 = MemAlloc2D(fhSize, fwSize, 0);
 
     FILE *inFile;
-    fopen_s(&inFile, "../LeerImagen/imagen/circulo.jpg", "rb");
+    fopen_s(&inFile, "../LeerImagen/imagen/cuadrado.jpg", "rb");
 
     if (inFile == NULL)
     {
@@ -34,33 +37,45 @@ int main()
         fread(input_img[h], sizeof(unsigned char), im.width(), inFile);
     }
 
-    for (int h = 0; h < 16; h++)
+    for (int h = 0; h < fhSize; h++)
         {
-            for (int w = 0; w < 16; w++)
+            for (int w = 0; w < fwSize; w++)
             {
-                double h_Ori = h/(16/ im.height());
-                double w_Ori = w/(16/im.width());
-                if (isInsideBoundary(16, 16,16/ im.height(), 16/im.width()))
+                double h_Ori1 = float(fhSize)/ im.height();
+                double h_Ori = h/h_Ori1;
+                double w_Ori1 = float(fwSize)/im.width();
+                double w_Ori = h/w_Ori1;
+
+                if (isInsideBoundary(fhSize, fwSize,fhSize/ im.height(), fwSize/im.width()))
                 {
                     output_img16[h][w] = BilinearInterpolation(input_img, height, width, h_Ori, w_Ori);
                 }
             }
         }
 
-        MemFree2D(input_img, im.height() > im.width()? im.height() : im.width());
-        MemFree2D(output_img16, 16);
-        fclose(inFile);
+    int ra[fhSize][fwSize];
+    int ga[fhSize][fwSize];
+    int ba[fhSize][fwSize];
 
-     for(int indx=0;indx<im.width();++indx){
-        for(int indy=0;indy < im.height();++indy){
+     for(int indx=0;indx<fhSize;++indx){
+        for(int indy=0;indy < fwSize;++indy){
             cout<<indx<<","<<indy<<"Rojo: "<<im.pixelColor(indx,indy).red()<<endl;
             cout<<indx<<","<<indy<<"verde: "<<im.pixelColor(indx,indy).green()<<endl;
             cout<<indx<<","<<indy<<"azul: "<<im.pixelColor(indx,indy).blue()<<endl;
 
+            ra[indx][indy] = im.pixelColor(indx,indy).red();
+            ga[indx][indy] = im.pixelColor(indx,indy).green();
+            ba[indx][indy] = im.pixelColor(indx,indy).blue();
+
+
         }
     }
+     MemFree2D(input_img, im.height() > im.width()? im.height() : im.width());
+     MemFree2D(output_img16, fhSize > fwSize? fhSize : fwSize);
+     fclose(inFile);
 
 
     return 0;
 };
+
 
